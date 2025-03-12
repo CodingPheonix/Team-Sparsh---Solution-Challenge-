702bc0ffc11343ec1efb3be2880fe1f9827e3f3d
Sohomdas2004 <sohomdas1510@gmail.com>
2025-03-12 11:06:30 +0530

    Created Basic Django app

---
+++ Backend/project/Hello/home/urls.py
@@ -0,0 +1,14 @@
+# from django.contrib import admin
+# from django.urls import path,include
+
+# urlpatterns = [
+#     path("admin/",admin.site.urls),
+#     path(" ",include("hpme.urls")
+# ]
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("home.urls")),  
+]
